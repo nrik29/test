2013ddcda606c5c178fee39f0bb67c2e2e02dcc1
nrik29 <32063034+nrik29@users.noreply.github.com>
2018-08-20 16:01:41 +0800

    cgngcn

--- liff-starter.js
+++ liff-starter.js
@@ -5,24 +5,24 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+    //document.getElementById('languagefield').textContent = data.language;
+    //document.getElementById('viewtypefield').textContent = data.context.viewType;
+    //document.getElementById('useridfield').textContent = data.context.userId;
+    //document.getElementById('utouidfield').textContent = data.context.utouId;
+    //document.getElementById('roomidfield').textContent = data.context.roomId;
+    //document.getElementById('groupidfield').textContent = data.context.groupId;
 
     // openWindow call
-    document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.openWindow({
-            url: 'https://line.me'
-        });
-    });
+    //document.getElementById('openwindowbutton').addEventListener('click', function () {
+    //    liff.openWindow({
+     //       url: 'https://line.me'
+    //    });
+    //});
 
     // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
+    //document.getElementById('closewindowbutton').addEventListener('click', function () {
+    //    liff.closeWindow();
+    //});
 
     // sendMessagesButton call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
@@ -86,33 +86,33 @@ function initializeApp(data) {
     });
     
     //get profile call https://www.google.co.id/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
+    //document.getElementById('getprofilebutton').addEventListener('click', function () {
+    //    liff.getProfile().then(function (profile) {
+    //        document.getElementById('useridprofilefield').textContent = profile.userId;
+     //       document.getElementById('displaynamefield').textContent = profile.displayName;
 
-            var profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            var img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
+    //        var profilePictureDiv = document.getElementById('profilepicturediv');
+    //        if (profilePictureDiv.firstElementChild) {
+    //            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+    //        }
+     //       var img = document.createElement('img');
+     //       img.src = profile.pictureUrl;
+    //        img.alt = "Profile Picture";
+    //        profilePictureDiv.appendChild(img);
+//
+   //         document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+   //         toggleProfileData();
+   //     }).catch(function (error) {
+   //         window.alert("Error getting profile: " + error);
+   //     });
+   // });
 }
 
-function toggleProfileData() {
-    var elem = document.getElementById('profileinfo');
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
+//function toggleProfileData() {
+//    var elem = document.getElementById('profileinfo');
+//    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+//        elem.style.display = "none";
+//    } else {
+//        elem.style.display = "block";
+//    }
+//}
