974713eabb50af4de806f0c995a668830b5a22db
nrik29 <32063034+nrik29@users.noreply.github.com>
2018-08-24 21:03:49 +0800

    hshshb

--- liff-starter.js
+++ liff-starter.js
@@ -5,6 +5,26 @@ window.onload = function (e) {
     });
 };
 
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+    }
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+       toggleModal();
+      }
+    }
+function modal(){
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+    }
+
+
 function getP(){
     var tipe = getParameterByName('type')
     if (!tipe) {
