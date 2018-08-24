b41ed91104aef227412b003606fda8ce97e22df4
nrik29 <32063034+nrik29@users.noreply.github.com>
2018-08-24 21:08:44 +0800

    hshdhdjdg

--- liff-starter.js
+++ liff-starter.js
@@ -2,27 +2,27 @@ window.onload = function (e) {
     liff.init(function () {
         getProfile();
         getP();
+        modal();
     });
 };
+function modal(){
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
+    function toggleModal() {
     modal.classList.toggle("show-modal");
     }
 
-function windowOnClick(event) {
+    function windowOnClick(event) {
     if (event.target === modal) {
        toggleModal();
       }
     }
-function modal(){
     trigger.addEventListener("click", toggleModal);
     closeButton.addEventListener("click", toggleModal);
     window.addEventListener("click", windowOnClick);
-    }
+}
 
 
 function getP(){
