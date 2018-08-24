36a2e6f63d121f94ac570e58cb04d9250d4229b3
nrik29 <32063034+nrik29@users.noreply.github.com>
2018-08-24 11:56:01 +0800

    tgggg

--- liff-starter.js
+++ liff-starter.js
@@ -31,6 +31,15 @@ function getProfile(){
             liff.closeWindow();
         });
     });
+$('#md-trigger').on('click', function(e) {
+      $('#modal-1').toggleClass("md-show"); //you can list several class names 
+      e.preventDefault();
+    });
+
+$('#md-close').on('click', function(e) {
+      $('#modal-1').toggleClass("md-show"); //you can list several class names 
+      e.preventDefault();
+    });
 }
 
 function makeList(){
